e60c0a140b67ae01cc58869cb41ce961f6d76bb0
Manas Sahoo <msahoo3103@gmail.com>
2025-11-02 23:50:15 +0530

    the library class has been initialized more methods still needs to be added

--- Digital_library_project/oob_test.py
+++ Digital_library_project/oob_test.py
@@ -1,9 +1,20 @@
 from Digital_library import *
 
-Book1 = Book("The Nun", "Manas Ranjan Sahoo", 123-456 , True)
-Book2 = Book("The Moron", "Sandeep Pradhan", 560-204 , False)
+The_Nun = Book("The Nun", "Manas Ranjan Sahoo", 123456 , True)
+The_Moron = Book("The Moron", "Sandeep Pradhan", 560204 , False)
 
 
 
-print(Book1)
-print(Book2)
+print(The_Nun)
+print(The_Moron)
+
+print(The_Nun.is_available())
+print(The_Moron.is_available())
+
+The_Moron.mark_unavailable()
+The_Nun.mark_unavailable()
+The_Nun.mark_available()
+The_Nun.mark_available()
+
+Library = Library()
+Library.add_book(The_Nun)
